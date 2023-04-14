4ad8a2b63b78a7e43a773bc4966ea9bcf0397859
Sabri ABDESSELEM <sabri.abdesselem@etu.iut-tlse3.fr>
2023-04-14 18:24:39 +0200

    en

--- app/src/main/java/com/example/zerodechets/view/AppActivity.java
+++ app/src/main/java/com/example/zerodechets/view/AppActivity.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.zerodechets.R;
-import com.google.android.material.snackbar.Snackbar;
 
 public class AppActivity extends AppCompatActivity  {
 
